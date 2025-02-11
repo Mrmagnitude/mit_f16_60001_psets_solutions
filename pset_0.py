269b965b64806394352bfe2f829b67ffe6784f56
Mrmagnitude <116489864+Mrmagnitude@users.noreply.github.com>
2025-02-11 12:37:10 +0100

    Add files via upload

---
+++ pset_0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 12:31:33 2025
+
+@author: Mr_Magnitude
+"""
+
+#pset 0
+# start by importing the python math module
+import math
+#getting inputs from the users and matching them to variable x and y
+x =int(input('enter a number for x: '))
+y = int(input('enter a number for y : '))
+# printing the product of x and y to the console
+print(x**y)
+# printing the value of the log of x in base 2 to the console
+print(math.log(x,2))
